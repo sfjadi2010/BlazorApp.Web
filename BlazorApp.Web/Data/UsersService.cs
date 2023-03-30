7cb98a70232d1f561ce40efb7c6c67ab8ee74547
Salman Farsi <salman.farsi@3together.us>
2023-03-30 12:16:33 -0400

    Add project files.

---
+++ BlazorApp.Web/Data/UsersService.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace BlazorApp.Web.Data
+{
+    public class UsersService
+    {
+        public async Task<User> GetUsersAsync()
+        {
+            using (HttpClient httpClient = new HttpClient()
+            {
+                BaseAddress = new Uri("https://reqres.in/api/")
+            })
+            {
+                HttpResponseMessage response = await httpClient.GetAsync("users");
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadFromJsonAsync<User>();
+
+                    if (result != null)
+                    {
+                        return result;
+                    }
+                }
+            }
+            return new User();
+        }
+    }
+}
